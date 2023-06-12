6af143452fcb01f69b2db1b278fe78dc6f41b364
ChrisMent <120587060+ChrisMent@users.noreply.github.com>
2023-06-12 21:41:44 +0200

    fix category scroll#

--- scripts/eventhandling.js
+++ scripts/eventhandling.js
@@ -10,31 +10,19 @@ function closeMenuOverlay(){
 
 //! Horizontal Scrollbar
 
-window.onload = function() {
-    var scrollAmount = 100; // Anzahl der Pixel, um die gescrollt werden soll
-    var scrollDuration = 200; // Dauer des Scrollvorgangs in Millisekunden
+const outsider = document.getElementById('outsider');
+const distance = 200;
 
-    var listElement = document.querySelector('.swiper-wrapper');
-    var scrollRightButton = document.getElementById('category-scroll-right');
-    var scrollLeftButton = document.getElementById('category-scroll-left');
-
-    scrollRightButton.addEventListener('click', function() {
-        listElement.scrollBy({
-            top: 0,
-            left: scrollAmount,
-            behavior: 'smooth'
-        });
-    });
-
-    scrollLeftButton.addEventListener('click', function() {
-        listElement.scrollBy({
-            top: 0,
-            left: -scrollAmount,
-            behavior: 'smooth'
-        });
-    });
+function scrollLeft() {
+  outsider.scrollBy({
+    left: -distance,
+    behavior: 'smooth'
+  });
 }
 
-
-
-
+function scrollRight() {
+  outsider.scrollBy({
+    left: distance,
+    behavior: 'smooth'
+  });
+}
