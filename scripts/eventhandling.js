77d975cdfd66a1ccfea74771357da2777b0f3f95
Christian Ment <christian.paul.ment@gmail.com>
2023-06-12 17:48:14 +0200

    horizonal scrolling

--- scripts/eventhandling.js
+++ scripts/eventhandling.js
@@ -8,3 +8,33 @@ function closeMenuOverlay(){
     menuOverlayClose.classList.add('hidden');
 }
 
+//! Horizontal Scrollbar
+
+window.onload = function() {
+    var scrollAmount = 100; // Anzahl der Pixel, um die gescrollt werden soll
+    var scrollDuration = 200; // Dauer des Scrollvorgangs in Millisekunden
+
+    var listElement = document.querySelector('.swiper-wrapper');
+    var scrollRightButton = document.getElementById('category-scroll-right');
+    var scrollLeftButton = document.getElementById('category-scroll-left');
+
+    scrollRightButton.addEventListener('click', function() {
+        listElement.scrollBy({
+            top: 0,
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+
+    scrollLeftButton.addEventListener('click', function() {
+        listElement.scrollBy({
+            top: 0,
+            left: -scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+}
+
+
+
+
