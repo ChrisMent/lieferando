8e1f5d57bfa6b432c368c1addf5f030400c4b8c6
ChrisMent <120587060+ChrisMent@users.noreply.github.com>
2023-06-19 12:01:06 +0200

    finishing menu

--- scripts/eventhandling.js
+++ scripts/eventhandling.js
@@ -9,70 +9,139 @@ function closeMenuOverlay(){
 }
 
 //! Horizontal Scrollbar
-document.addEventListener("DOMContentLoaded", function() {
-    let scrollAmount = 100; // Anzahl der Pixel, um die gescrollt werden soll
-    
-    let listElement = document.getElementById('outsider');
-    let scrollRightButton = document.getElementById('category-scroll-right');
-    let scrollLeftButton = document.getElementById('category-scroll-left');
-    
-    let hasScrolledRight = false; // Variable, um zu verfolgen, ob nach rechts gescrollt wurde
-    
-    // Funktion, um den Status der Scroll-Buttons zu aktualisieren
-    function updateScrollButtons() {
-        let scrollLeft = listElement.scrollLeft;
-        let maxScrollLeft = listElement.scrollWidth - listElement.clientWidth;
-    
-        // Versteckt den rechten Scroll-Button und setzt overflow auf visible, wenn das Ende der Liste erreicht ist und nach rechts gescrollt wurde
-        if (scrollLeft >= maxScrollLeft - scrollAmount && hasScrolledRight) {
-            scrollRightButton.style.visibility = 'hidden';
-            listElement.style.overflow = 'visible';
-        } else {
-            scrollRightButton.style.visibility = 'visible';
-            if (!hasScrolledRight) {
-                listElement.style.overflow = 'auto';
-            }
-        }
-    
-        // Versteckt den linken Scroll-Button, wenn der Anfang der Liste erreicht ist
-        if (scrollLeft <= scrollAmount) {
-            scrollLeftButton.style.visibility = 'hidden';
-            listElement.style.overflow = 'auto';
-        } else {
-            scrollLeftButton.style.visibility = 'visible';
-        }
+        // Fügt einen Event-Listener hinzu, der ausgeführt wird, wenn das DOM vollständig geladen ist
+        window.addEventListener('DOMContentLoaded', (event) => {
+            // Definiert die Elemente, die manipuliert werden sollen
+            const scrollContent = document.getElementById('outsider');
+            const scrollLeftButton = document.getElementById('category-scroll-left');
+            const scrollRightButton = document.getElementById('category-scroll-right');
+
+            // Funktion, die überprüft, ob die Scroll-Position am Anfang oder am Ende der Liste ist
+            // und die Sichtbarkeit der Buttons entsprechend ändert
+            const checkButtonVisibility = () => {
+                // Ermittelt die aktuelle Scroll-Position und das Maximum, das gescrollt werden kann
+                const scrollPosition = scrollContent.scrollLeft;
+                const scrollMax = scrollContent.scrollWidth - scrollContent.clientWidth;
+
+                // Wenn die Scroll-Position 0 oder kleiner ist, wird der linke Button ausgeblendet, sonst wird er angezeigt
+                scrollLeftButton.style.visibility = (scrollPosition <= 0) ? 'hidden' : 'visible';
+                // Wenn die Scroll-Position gleich oder größer als das Maximum ist, wird der rechte Button ausgeblendet, sonst wird er angezeigt
+                scrollRightButton.style.visibility = (scrollPosition >= scrollMax) ? 'hidden' : 'visible';
+            };
+
+            // Fügt einen Event-Listener für das Klick-Event des linken Buttons hinzu
+            // Wenn der Button geklickt wird, scrollt die Liste 100px nach links
+            scrollLeftButton.addEventListener('click', () => {
+                scrollContent.scrollBy({ left: -100, behavior: 'smooth' });
+            });
+
+            // Fügt einen Event-Listener für das Klick-Event des rechten Buttons hinzu
+            // Wenn der Button geklickt wird, scrollt die Liste 100px nach rechts
+            scrollRightButton.addEventListener('click', () => {
+                scrollContent.scrollBy({ left: 100, behavior: 'smooth' });
+            });
+
+            // Fügt einen Event-Listener für das Mausrad hinzu
+            scrollContent.addEventListener('wheel', (event) => {
+                // Verhindert das Standardverhalten des Mausrads
+                event.preventDefault();
+                // Scrollt die Liste horizontal basierend auf der Drehrichtung des Mausrads
+                scrollContent.scrollBy({ left: event.deltaY, behavior: 'smooth' });
+            });
+
+            // Variablen für die Drag-and-Drop-Funktionalität
+            let isDown = false;
+            let startX;
+            let scrollLeft;
+
+            scrollContent.addEventListener('mousedown', (e) => {
+                isDown = true;
+                startX = e.pageX - scrollContent.offsetLeft;
+                scrollLeft = scrollContent.scrollLeft;
+            });
+
+            scrollContent.addEventListener('mouseleave', () => {
+                isDown = false;
+            });
+
+            scrollContent.addEventListener('mouseup', () => {
+                isDown = false;
+            });
+
+            scrollContent.addEventListener('mousemove', (e) => {
+                if (!isDown) return;
+                e.preventDefault();
+                const x = e.pageX - scrollContent.offsetLeft;
+                const walk = (x - startX) * 1; // scroll-fast
+                scrollContent.scrollLeft = scrollLeft - walk;
+            });
+
+            // Fügt einen Event-Listener für das Scroll-Event der Liste hinzu
+            // Wenn die Liste gescrollt wird, wird die Funktion zur Überprüfung der Button-Sichtbarkeit aufgerufen
+            scrollContent.addEventListener('scroll', checkButtonVisibility);
+
+            // Fügt einen Event-Listener für das Klick-Event jedes Listenelements hinzu
+            const listItems = scrollContent.querySelectorAll('.swiper-wrapper-container');
+            listItems.forEach(item => {
+                item.addEventListener('click', () => {
+                    // Entfernt die is-active Klasse von allen Listenelementen
+                    listItems.forEach(i => i.classList.remove('is--active'));
+                    // Fügt die is-active Klasse zum angeklickten Element hinzu
+                    item.classList.add('is--active');
+                });
+            });
+
+            // Ruft die Funktion zur Überprüfung der Button-Sichtbarkeit auf, wenn die Seite geladen wird
+            checkButtonVisibility();
+        });
+
+
+        // Entfernen der Klasse "d-none" nach Klicken
+
+
+
+function openSearchOverlay() {
+    let openSearch = document.getElementById('search-modal-toggle')
+    openSearch.classList.remove('d-none');
+}
+
+function closeSearchOverlay() {
+    let closeSearch = document.getElementById('search-modal-toggle')
+    closeSearch.classList.add('d-none');
+
+}
+
+// Definieren Sie die IDs des Sentinel und des Containers, und die Ziel-Klasse
+const sentinelId = 'menu-sentinel';
+const containerClass = 'restaurant-menu-nav';
+const shadowClass = 'restaurant-menu-nav-shadow';
+
+// Erstellen Sie einen Observer, der eine Funktion ausführt, wenn das Sentinel im oder außerhalb des Viewports ist
+let observer = new IntersectionObserver(function(entries) {
+  // Iteriere durch alle Einträge
+  entries.forEach(function(entry) {
+    // Wenn das Sentinel im Viewport ist
+    if (entry.isIntersecting) {
+      // Fügen Sie den Schatten zum Container hinzu
+      document.querySelector(`.${containerClass}`).classList.add(shadowClass);
+    } else {
+      // Wenn das Sentinel nicht im Viewport ist, entfernen Sie den Schatten vom Container
+      document.querySelector(`.${containerClass}`).classList.remove(shadowClass);
     }
-    
-    // Aktualisiert den Status der Scroll-Buttons, wenn die Seite geladen wird
-    updateScrollButtons();
-    
-    scrollRightButton.addEventListener('click', function() {
-        listElement.scrollBy({
-            top: 0,
-            left: scrollAmount,
-            behavior: 'smooth'
-        });
-    
-        hasScrolledRight = true; // Setzt hasScrolledRight auf true, wenn nach rechts gescrollt wurde
-    
-        // Aktualisiert den Status der Scroll-Buttons nach dem Scrollen
-        setTimeout(updateScrollButtons, 200); // Verzögerung, um das Scrollen abzuschließen
-    });
-    
-    scrollLeftButton.addEventListener('click', function() {
-        listElement.scrollBy({
-            top: 0,
-            left: -scrollAmount,
-            behavior: 'smooth'
-        });
-    
-        hasScrolledRight = false; // Setzt hasScrolledRight auf false, wenn nach links gescrollt wurde
-    
-        // Aktualisiert den Status der Scroll-Buttons nach dem Scrollen
-        setTimeout(updateScrollButtons, 200); // Verzögerung, um das Scrollen abzuschließen
-    });
-    
+  });
+}, { threshold: [0] });  // Auslösen, wenn 0% des Ziels sichtbar sind
 
+// Beobachten Sie das Sentinel
+observer.observe(document.querySelector(`#${sentinelId}`));
+
+const menuNav = document.querySelector('.restaurant-menu-nav');
+const menuWrapper = document.querySelector('.restaurant-menu-wrapper');
+
+menuWrapper.addEventListener('scroll', function() {
+  if (menuWrapper.scrollTop > 0) {
+    menuNav.classList.add('has-shadow');
+  } else {
+    menuNav.classList.remove('has-shadow');
+  }
 });
-
 
